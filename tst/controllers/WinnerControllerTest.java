b137410fbc006d1be7e4b9ce3ebd6643d03da83e
b.pershin <borpernic@gmail.com>
2016-04-08 00:44:13 +0300

    WinnerController Test changed

--- tst/controllers/WinnerControllerTest.java
+++ tst/controllers/WinnerControllerTest.java
@@ -6,22 +6,61 @@ import org.junit.Test;
 
 import java.awt.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by b.pershin on 07.04.2016.
  */
 public class WinnerControllerTest {
     @Test
-    public void getWinner() throws Exception {
-        Field field=new Field();
-        field.setFigure(new Point(0,0),Figure.X);
-        field.setFigure(new Point(1,0),Figure.X);
-        field.setFigure(new Point(3,0),Figure.X);
+    public void getWinnerCol() throws Exception {
+        Field field = new Field();
         WinnerController winnerController = new WinnerController();
-
-
-        assertEquals(winnerController.getWinner(field),Figure.X);
+        for (int i = 0; i < 3; i++) {
+            field.setFigure(new Point(0, i), Figure.X);
+            field.setFigure(new Point(1, i), Figure.X);
+            field.setFigure(new Point(2, i), Figure.X);
+            assertEquals(winnerController.getWinner(field), Figure.X);
+        }
     }
 
+
+    @Test
+    public void getWinnerRow() throws Exception {
+        Field field = new Field();
+        WinnerController winnerController = new WinnerController();
+        for (int i = 0; i < 3; i++) {
+            field.setFigure(new Point(i, 0), Figure.X);
+            field.setFigure(new Point(i, 1), Figure.X);
+            field.setFigure(new Point(i, 2), Figure.X);
+            assertEquals(winnerController.getWinner(field), Figure.X);
+        }
+    }
+
+    @Test
+    public void getWinnerDiag1() throws Exception {
+        Field field = new Field();
+        WinnerController winnerController = new WinnerController();
+        for (int i = 0; i < 3; i++) {
+           field.setFigure(new Point(i, i), Figure.X);
+
+      /*      field.setFigure(new Point(0, 0), Figure.X);
+            field.setFigure(new Point(1, 1), Figure.X);
+            field.setFigure(new Point(2, 2), Figure.X);*/
+            //assertEquals(winnerController.getWinner(field), Figure.X);
+
+        }
+            assertEquals(winnerController.getWinner(field), Figure.X);
+    }
+
+    @Test
+    public void getWinnerDiag2() throws Exception {
+        Field field = new Field();
+        WinnerController winnerController = new WinnerController();
+        field.setFigure(new Point(0, 2), Figure.X);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 0), Figure.X);
+        assertEquals(winnerController.getWinner(field), Figure.X);
+
+    }
 }
